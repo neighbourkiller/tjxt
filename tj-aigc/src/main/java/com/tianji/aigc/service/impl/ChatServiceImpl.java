f3d6407f824c8b214268e72ac96ea28d29e549a8
neighbourkiller <3319578099@qq.com>
2026-08-12 12:31:02 +0800

    修改腾讯云对象存储配置并实现系统提示词

--- tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -1,8 +1,10 @@
 package com.tianji.aigc.service.impl;
 
+import com.tianji.aigc.config.SystemPromptConfig;
 import com.tianji.aigc.enums.ChatEventTypeEnum;
 import com.tianji.aigc.service.ChatService;
 import com.tianji.aigc.vo.ChatEventVO;
+import com.tianji.common.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -16,26 +18,27 @@ import reactor.core.publisher.Flux;
 public class ChatServiceImpl implements ChatService {
 
     private final ChatClient chatClient;
+    private final SystemPromptConfig systemPromptConfig;
 
     @Override
     public Flux<ChatEventVO> chat(String question, String sessionId) {
         return this.chatClient.prompt()
+                .system(promptSystem ->{promptSystem
+                        .text(this.systemPromptConfig.getChatSystemMessage().get()) // 设置系统提示语
+                        .param("now", DateUtils.now()); // 设置当前时间参数
+                } )
                 .user(question)
                 .stream()
                 .chatResponse()
                 .<ChatEventVO>handle((chatResponse, sink) -> {
-                    if (chatResponse.getResult() == null
-                            || chatResponse.getResult().getOutput() == null) {
+                    if (chatResponse.getResult() == null || chatResponse.getResult().getOutput() == null) {
                         return;
                     }
-
                     String text = chatResponse.getResult()
                             .getOutput()
                             .getText();
-
-                    if (text == null || text.isEmpty()) {
+                    if (text == null || text.isEmpty())
                         return;
-                    }
 
                     sink.next(ChatEventVO.builder()
                             .eventData(text)
